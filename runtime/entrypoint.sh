3571336fe09f71d2ade86766477a4f305ba17307
Isaac Slavitt <isaac.slavitt@gmail.com>
2020-12-10 23:09:08 -0500

    Updates to entrypoint for checking files and running main loop

--- runtime/entrypoint.sh
+++ runtime/entrypoint.sh
@@ -7,27 +7,36 @@ exit_code=0
 
     echo "Unpacking submission..."
     unzip ./submission/submission.zip -d ./
-
     if [ -f "main.py" ]
     then
+        echo "ERROR: main.py is not user supplied; remove this from your submission."
+        exit_code=1
+
+    elif [ ! -f "predict.py" ]
+    then
+        echo "ERROR: predict.py does not exist."
+        exit_code=1
+    else
+
+        echo "Copying main.py"
+        cp ./data/main.py ./
+        echo "... main.py copied $(md5sum ./main.py)"
+
         echo "Running submission with Python"
         conda run -n py python main.py
-    
-    else
-        echo "ERROR: Could not find main.py in submission.zip"
-        exit_code=1
-    fi
 
-    echo "Exporting submission.csv result..."
+        echo "Exporting submission.csv result..."
 
-    # Valid scripts must create a "submission.csv" file within the same directory as main
-    if [ -f "submission.csv" ]
-    then
-        echo "Script completed its run."
-        cp submission.csv ./submission/submission.csv
-    else
-        echo "ERROR: Script did not produce a submission.csv file in the main directory."
-        exit_code=1
+        # Valid scripts must create a "submission.csv" file within the same directory as main
+        if [ -f "submission.csv" ]
+        then
+            echo "Script completed its run."
+            cp submission.csv ./submission/submission.csv
+        else
+            echo "ERROR: Script did not produce a submission.csv file in the main directory."
+            exit_code=1
+        fi
+
     fi
 
     echo "================ END ================"
