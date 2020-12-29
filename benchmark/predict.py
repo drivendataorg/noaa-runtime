8116d68a263192d763cafc7b992fc510adfadb02
Sammoura <samer.ayad.ayoub@gmail.com>
2020-12-29 18:41:37 -0500

    Update predict.py

--- benchmark/predict.py
+++ benchmark/predict.py
@@ -1,7 +1,98 @@
-import pandas as pd
+import json
+import pickle
 from typing import Tuple
 
+import keras
+import pandas as pd
 
+# Load in serialized model, config, and scaler
+model = keras.models.load_model("model.h5")
+
+with open("config.json", "r") as f:
+    CONFIG = json.load(f)
+
+with open("scaler.pck", "rb") as f:
+    scaler = pickle.load(f)
+
+# Set global variables    
+TIMESTEPS = CONFIG["timesteps"]
+SOLAR_WIND_FEATURES = [
+    "bt",
+    "temperature",
+    "bx_gse",
+    "by_gse",
+    "bz_gse",
+    "speed",
+    "density",
+]
+XCOLS = (
+    [col + "_mean" for col in SOLAR_WIND_FEATURES]
+    + [col + "_std" for col in SOLAR_WIND_FEATURES]
+    + ["smoothed_ssn"]
+)
+
+
+# Define functions for preprocessing
+def impute_features(feature_df):
+    """Imputes data using the following methods:
+    - `smoothed_ssn`: forward fill
+    - `solar_wind`: interpolation
+    """
+    # forward fill sunspot data for the rest of the month
+    feature_df.smoothed_ssn = feature_df.smoothed_ssn.fillna(method="ffill")
+    # interpolate between missing solar wind values
+    feature_df = feature_df.interpolate()
+    return feature_df
+
+
+def aggregate_hourly(feature_df, aggs=["mean", "std"]):
+    """Aggregates features to the floor of each hour using mean and standard deviation.
+    e.g. All values from "11:00:00" to "11:59:00" will be aggregated to "11:00:00".
+    """
+    # group by the floor of each hour use timedelta index
+    agged = feature_df.groupby(
+        ["period", feature_df.index.get_level_values(1).floor("H")]
+    ).agg(aggs)
+    # flatten hierachical column index
+    agged.columns = ["_".join(x) for x in agged.columns]
+    return agged
+
+
+def preprocess_features(solar_wind, sunspots, scaler=None, subset=None):
+    """
+    Preprocessing steps:
+        - Subset the data
+        - Aggregate hourly
+        - Join solar wind and sunspot data
+        - Scale using standard scaler
+        - Impute missing values
+    """
+    # select features we want to use
+    if subset:
+        solar_wind = solar_wind[subset]
+
+    # aggregate solar wind data and join with sunspots
+    hourly_features = aggregate_hourly(solar_wind).join(sunspots)
+
+    # subtract mean and divide by standard deviation
+    if scaler is None:
+        scaler = StandardScaler()
+        scaler.fit(hourly_features)
+
+    normalized = pd.DataFrame(
+        scaler.transform(hourly_features),
+        index=hourly_features.index,
+        columns=hourly_features.columns,
+    )
+
+    # impute missing values
+    imputed = impute_features(normalized)
+
+    # we want to return the scaler object as well to use later during prediction
+    return imputed, scaler
+
+
+# THIS MUST BE DEFINED FOR YOUR SUBMISSION TO RUN
 def predict_dst(
     solar_wind_7d: pd.DataFrame,
     satellite_positions_7d: pd.DataFrame,
@@ -10,29 +101,39 @@ def predict_dst(
     """
     Take all of the data up until time t-1, and then make predictions for
     times t and t+1.
-
     Parameters
     ----------
     solar_wind_7d: pd.DataFrame
-        The last 7 days of satellite data up until (t - 1) minutes [exclusive of t], with `timedelta` as index
+        The last 7 days of satellite data up until (t - 1) minutes [exclusive of t]
     satellite_positions_7d: pd.DataFrame
-        The last 7 days of satellite position data up until the present time [inclusive of t], with `timedelta` as index
+        The last 7 days of satellite position data up until the present time [inclusive of t]
     latest_sunspot_number: float
         The latest monthly sunspot number (SSN) to be available
-
     Returns
     -------
     predictions : Tuple[float, float]
         A tuple of two predictions, for (t and t + 1 hour) respectively; these should
         be between -2,000 and 500.
     """
+    # Re-format data to fit into our pipeline
+    sunspots = pd.DataFrame(index=solar_wind_7d.index, columns=["smoothed_ssn"])
+    sunspots["smoothed_ssn"] = latest_sunspot_number
+    
+    # Process our features and grab last 32 (timesteps) hours
+    features, s = preprocess_features(
+        solar_wind_7d, sunspots, scaler=scaler, subset=SOLAR_WIND_FEATURES
+    )
+    model_input = features[-TIMESTEPS:][XCOLS].values.reshape(
+        (1, TIMESTEPS, features.shape[1])
+    )
+    
+    # Make a prediction
+    prediction_at_t0, prediction_at_t1 = model.predict(model_input)[0]
 
-    ########################################################################
-    #                         YOUR CODE HERE!                              #
-    ########################################################################
-
-    # this is a naive baseline where we just guess the training data mean every time
-    prediction_at_t0 = -12
-    prediction_at_t1 = -12
+    # Optional check for unexpected values
+    if not np.isfinite(prediction_at_t0):
+        prediction_at_t0 = -12
+    if not np.isfinite(prediction_at_t1):
+        prediction_at_t1 = -12
 
     return prediction_at_t0, prediction_at_t1
